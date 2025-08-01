5fcab15d6e82c741972630c3d3d26ef7bf54f339
maxpestr <maxpestr@gmail.com>
2025-08-01 17:12:36 +0300

    +FinMrkt -OldVibr
    Reorganisation FinMrkt deleted old Vibrations files

--- Vibrations/Vib/toexcel3.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-numbers = [1, 2, 3, 4, 5]
-
-# Create a DataFrame from the list of numbers
-df = pd.DataFrame(numbers, columns=['Numbers'])
-
-# Save the DataFrame to an Excel file
-df.to_excel('numbers.xlsx', index=False)
